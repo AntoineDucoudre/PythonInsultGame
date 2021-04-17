8340c3cee427993365a0dc2e0b503a61ce9555e7
TSOUMOU-BITA <72856494+TSOUMOU-BITA@users.noreply.github.com>
2021-04-17 02:44:57 +0200

    Add files via upload

---
+++ listesinsults.py
@@ -0,0 +1,17 @@
+
+Exclamationsfinales =['Hélas ?','Zut alors','Non vraiment.','Chut','on n\'entend plus rien','Viens la','Ouf','Oh',
+'C\'est fini','Voyons !',
+'avance mon petit âne','tu me fais mal !','Au secours','et c’est tout ce que j’aurai à dire !','Tiens !',
+'ça ne sent pas la rose dans cette cave','l’ingrat !','Et comment !','oui !','ça ne sent pas la rose dans cette cave.',
+'Comment tu me parles !','C’était faux ?','raconte pas ta vie']
+Complément = ['J\'ai crié à en perdre la voix.','tu rentres dans le rang ou tu dégages','Tu dégages',
+'fais gaffe à toi','Sale pourri','Ta barbe mal taillée',' Ça craint, sa race ']
+Liaisons = ['En premier lieu','En outre','d\'autre part','Car','il est nécessaire','en raison de','mais parce que',
+'dans la mesure ou','Sous pretexte que','d\'un autre côté','Mais or','Bien que','Puisque ','Il est important','En effet',
+'a defaut de ','on n\'oubliera pas que','sans doute'
+,'Neanmoins','en depit de','a présent','concretement','en revanche','toutefois',
+'etant donne que','au premier abord','non seulement','de meme','particulierement','a l\'inverse','de ce fait','D\'ou','par deduction',
+'Au contraire','certes','par ailleurs','maintenant','ainsi','Or','malgré']
+sujet = ['arrête ton char','Tu m’prends vraiment pour un pauvre mec','Je l’ai insulté sauvagement','Hey mon p’tit boudin comment ça va',
+'Ta mere','Ta soeur','Ton frere','Ton pere','Ton oncle le chauve','Ta Tante','Ton grand-pere','Ta grand mere',
+'ton fils en Y']
